6962137ff729ae9f761d9283aa15f2522e93ff28
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2025-08-31 14:09:04 -0400

    feat: values

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import styles from "./Partners.module.scss";
 import Image from "next/image";
 import link from "@/images/product-link.png";
+import logo from "../images/icon.png";
+import stats from "../images/stats-img.png";
 
 export default function Partners() {
   return (
@@ -19,12 +21,36 @@ export default function Partners() {
               </p>
 
               <Link className={styles.link} href={"/"}>
-                See catalog
+                contact us
                 <Image className={styles.btn} src={link} alt="link" />
               </Link>
             </div>
           </div>
-          <div className={styles.values}></div>
+          <div className={styles.values}>
+            <div className={styles.stats}>
+              <div className={styles.stats__info}>
+                <Image className={styles.stats__btn} src={logo} alt="link" />
+                <ul className={styles.list}>
+                  <li className={styles.item}>
+                    Direct deliveries of products from the manufacturer
+                  </li>
+                  <li className={styles.item}>
+                    Guaranteed traceability and certification
+                  </li>
+                  <li className={styles.item}>
+                    Individual approach to each partner
+                  </li>
+                </ul>
+              </div>
+              <Image className={styles.stats__img} src={stats} alt="link" />
+            </div>
+            <div className={styles.info}>
+              <p className={styles.info__text}>
+                We value long-term relationships and are open to new
+                opportunities for cooperation!
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
